0033e49cc9ed5e57c001a33e31de17c84d4e0785
Vadim Kravcov <v.sem4ik@mail.ru>
2023-09-03 09:07:21 +0300

    add first try

--- homework-3/queries.sql
+++ homework-3/queries.sql
@@ -1,16 +1,35 @@
 -- Напишите запросы, которые выводят следующую информацию:
 -- 1. Название компании заказчика (company_name из табл. customers) и ФИО сотрудника, работающего над заказом этой компании (см таблицу employees),
 -- когда и заказчик и сотрудник зарегистрированы в городе London, а доставку заказа ведет компания United Package (company_name в табл shippers)
-
+SELECT customers.company_name, CONCAT(first_name, ' ', last_name)
+from orders
+join customers USING(customer_id)
+join employees USING(employee_id)
+join shippers ON orders.ship_via=shippers.shipper_id
+where customers.city = 'London' and employees.city = 'London'
+and shippers.company_name = 'United Package'
 
 -- 2. Наименование продукта, количество товара (product_name и units_in_stock в табл products),
 -- имя поставщика и его телефон (contact_name и phone в табл suppliers) для таких продуктов,
 -- которые не сняты с продажи (поле discontinued) и которых меньше 25 и которые в категориях Dairy Products и Condiments.
 -- Отсортировать результат по возрастанию количества оставшегося товара.
-
+SELECT products.product_name, products.units_in_stock,
+suppliers.contact_name, suppliers.phone
+from products
+join suppliers USING(supplier_id)
+join categories USING(category_id)
+where products.discontinued <1 and products.units_in_stock < 25
+and categories.category_name IN ('Dairy Products','Condiments')
+ORDER BY units_in_stock ASC
 
 -- 3. Список компаний заказчиков (company_name из табл customers), не сделавших ни одного заказа
-
+SELECT company_name
+FROM customers
+LEFT JOIN orders USING(customer_id)
+WHERE order_id IS NULL
+ORDER BY company_name
 
 -- 4. уникальные названия продуктов, которых заказано ровно 10 единиц (количество заказанных единиц см в колонке quantity табл order_details)
 -- Этот запрос написать именно с использованием подзапроса.
+SELECT DISTINCT product_name FROM products
+WHERE product_id IN (SELECT product_id FROM order_details WHERE quantity = 10);
